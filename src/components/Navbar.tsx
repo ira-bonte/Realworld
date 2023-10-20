98812b31f6b8998308b8d4702893efd7556585c5
MUGANZA Jesus <muganzajesus001@gmail.com>
2023-10-20 19:27:14 +0200

    from jesu

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,15 +3,18 @@ import logo1 from '../assets/img/png/logo-no-background.png'
 import {BsFacebook,BsTwitter,BsInstagram,BsSearch} from 'react-icons/bs'
 const Navbar = () => {
   return (
-    <nav className='flex gap-48 bg-gray-50 text-black h-24 w-full fixed z-10 p-2'>
-          <img src={logo1} alt="" className='h-20 w-56 '/>
-        <ul className='flex gap-16 p-6 text-black text-xl'>
+    <nav className='flex gap-48 bg-gray-50 text-black h-16 w-full fixed z-10 p-2'>
+          <img src={logo1} alt="" className='h-12  '/>
+        <ul className='flex gap-16 p-3 text-black text-xl'>
           <li>Blog</li>
           <li>Single post</li>
           <li>Categories</li>
           </ul>
-        <div className='flex gap-8 p-6'>
-        <BsFacebook/>
+          {/* media */}
+        <div className='flex gap-8 p-3 text-2xl '>
+          <a href="./">
+            <BsFacebook/>
+          </a>
           <BsTwitter/>
           <BsInstagram/>
           <BsSearch/>
