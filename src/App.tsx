47f59fe05e56bff28ffdcda15aae0f4c2e1d5c6b
brunorwanda4 <brunorwanda4@gmail.com>
2023-10-22 19:02:52 +0200

    make slide image and videos

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,12 @@ import React from 'react'
 import Navbar from './components/Navbar'
 import Topnews from './components/news/topnews'
 import Footer from './components/footer'
-
+import Hero  from './components/Hero'
 const App = () => {
   return (
     <div>
       <Navbar/>
+      <Hero/>
       <Topnews/>
       <Footer/>
     </div>
---
+++ src/components/Hero/BbcNews.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import bbcNewOne from '../../assets/img/news/bbcNews-1.jpg'
+
+const BbcNews = () => {
+  const className = {
+    imgContainer: "h-[85hv] w-[12hv] relative",
+    img: "h-full w-full object-cover cursor-pointer rounded-md",
+    
+  }
+
+  return (
+    <div>
+      {/* background image */}
+      <a href="./">
+        <img src={bbcNewOne} alt="" className={className.img} />
+      </a>
+    </div>
+  )
+}
+
+export default BbcNews
