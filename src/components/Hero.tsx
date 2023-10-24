1e475a9cbd78c4d19cfa287b42d2856056068d18
brunorwanda4 <brunorwanda4@gmail.com>
2023-10-24 16:11:29 +0200

    make hero and top news

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -5,8 +5,8 @@ import Slider from "react-slick"
 import IgiheSirider from './Hero/IgiheSirider'
 import BbcNews from './Hero/BbcNews'
 // icons
-import { BsArrowDownRight } from "react-icons/bs"
 import { FaChevronLeft, FaChevronRight } from 'react-icons/fa'
+import Popular from './Hero/Popular'
 // start
 const Hero = () => {
     const sliderRef = useRef(null);
@@ -30,25 +30,43 @@ const Hero = () => {
         sliderRef.current.slickNext();
       };
   return (
-    <div className= " relative pt-20 px-2">
-      <Slider ref={sliderRef} {...settings}>
-        <div>
-          <BbcNews/>
+    <section className= " relative pt-20 px-2 flex">
+      <div  className=' h-[450px] w-[990px]'>
+        <Slider ref={sliderRef} {...settings}>
+          <div>
+            <BbcNews/>
+          </div>
+          <div>
+            <IgiheSirider/>
+          </div>
+        </Slider>
+        <div className=' flex w-[990px] justify-between absolute z-40 px-2 -mt-64'>
+        <button className=' text-2xl text-white bg-amber-950 h-8 w-8 text-center rounded-full cursor-pointer hover:scale-110 duration-300  hover:bg-amber-500' onClick={goToPrevSlide}>
+            <FaChevronLeft/>
+          </button>
+          <button className=' text-2xl text-white bg-amber-950 h-8 w-8 text-center rounded-full cursor-pointer hover:scale-110 duration-300  hover:bg-amber-500' onClick={goToNextSlide}>
+            <FaChevronRight/>
+          </button>
         </div>
-        <div>
-          <IgiheSirider/>
-        </div>
-      </Slider>
-      {/* button of slider */}
-      <div className=' flex w-[990px] justify-between absolute z-40 px-2 -mt-64'>
-      <button className=' text-2xl text-white bg-amber-950 h-8 w-8 text-center rounded-full cursor-pointer hover:scale-110 duration-300  hover:bg-amber-500' onClick={goToPrevSlide}>
-          <FaChevronLeft/>
+      </div>
+     {/* other news */}
+     <div className='px-2'>
+      {/* categories of Popular*/}
+      <div className=' flex gap-4'>
+        <button className=' font-medium text-base hover:text-amber-500 duration-300 hover:border-b-2 border-amber-600'>
+          Popular
         </button>
-        <button className=' text-2xl text-white bg-amber-950 h-8 w-8 text-center rounded-full cursor-pointer hover:scale-110 duration-300  hover:bg-amber-500' onClick={goToNextSlide}>
-          <FaChevronRight/>
+        <button className=' font-medium text-base hover:text-amber-500 duration-300 hover:border-b-2 border-amber-600'>
+          Trending
+        </button>
+        <button className=' font-medium text-base hover:text-amber-500 duration-300 hover:border-b-2 border-amber-600'>
+          Videos
         </button>
       </div>
-    </div>
+      <Popular/>
+
+     </div>
+    </section>
   )
 }
 
---
+++ src/components/Hero/Popular.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Popular = () => {
+  return (
+    <div>
+      PopuLar
+    </div>
+  )
+}
+
+export default Popular
