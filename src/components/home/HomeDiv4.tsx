fe8abfa686956f788831627e4ee3cc4a4f69edf2
brunorwanda4 <brunorwanda4@gmail.com>
2023-10-26 20:29:58 +0200

    add teta news

--- src/components/home/HomeDiv4.tsx
+++ src/components/home/HomeDiv4.tsx
@@ -1,14 +1,19 @@
 import React from 'react'
-import {} from 'react-icons/bs'
+import {BsInstagram} from 'react-icons/bs'
 import {} from 'react-icons/fa'
+import {BiSolidBadgeCheck} from 'react-icons/bi'
+import rapLogo from '../../assets/img/instagram/rap/logo.png'
 import { NavLink } from 'react-router-dom'
 const HomeDiv4 = () => {
   return (
     <div>
       {/* instagram */}
       <div>
-        <a href='https://www.instagram.com/p/Cy3jAIHOV7b/?hl=en'>
-          
+        <a href='https://www.instagram.com/p/Cy3jAIHOV7b/?hl=en' className= " flex ">
+          <BsInstagram/>
+          <img src={rapLogo} alt="Rap profile" className=' w-12 h-12 object-cover ' />
+          <span>rap</span>
+          <BiSolidBadgeCheck/>
         </a>
       </div>
     </div>
