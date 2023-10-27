3bab3ad90d24da749674c05e3d20abff805cfac3
Iradukunda Bonte <irabonte123@gmail.com>
2023-10-26 18:30:02 -0700

    from bonte

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,9 @@
 import React from 'react'
-import Navbar from './components/Navbar'
-import Topnews from './components/news/topnews'
-import Footer from './components/footer'
-import Header from './components/Header';
-import Hero from './components/Hero';
 import NavRouters from './components/NavRouters'
 
 const App = () => {
   return (
     <section className=' bg-gray-100'>
-      <Navbar/>
-      <Hero/>
-      <Topnews/>
-      <Footer/>
-      <Header/>
       <NavRouters/>
       </section>
   )
